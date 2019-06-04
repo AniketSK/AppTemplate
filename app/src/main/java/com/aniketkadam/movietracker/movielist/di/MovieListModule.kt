e2bf3c68f377342fef486b59d27ff7d10da2d970
Aniket Kadam <anikadamg@gmail.com>
2019-06-04 18:04:21 +0530

    Removed a redundant declaration of how modelstore is created.

--- app/src/main/java/com/aniketkadam/movietracker/movielist/di/MovieListModule.kt
+++ app/src/main/java/com/aniketkadam/movietracker/movielist/di/MovieListModule.kt
@@ -1,12 +1,8 @@
 package com.aniketkadam.movietracker.movielist.di
 
-import com.aniketkadam.movietracker.movielist.ModelStore
-import com.aniketkadam.movietracker.movielist.network.MovieApis
 import dagger.Module
-import dagger.Provides
 
 @Module
 class MovieListModule {
-    @Provides
-    fun providesModelStore(movieApis: MovieApis) = ModelStore(movieApis)
+
 }
